3c805277e52d47ab43d199e3db07c26430bccd68
seungjun6235 <58593578+seungjun6235@users.noreply.github.com>
2023-11-10 10:46:48 +0900

    likes 기능 구현

--- posts/urls.py
+++ posts/urls.py
@@ -6,4 +6,6 @@ app_name = 'posts'
 urlpatterns =[
     path('',views.index,name='index'),
     path('create/',views.create,name='create'),
+    path('<int:id>/likes/',views.likes,name='likes'),
+
 ]
